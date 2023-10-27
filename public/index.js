9c4e6583b2a3ae78b604069c0970519888719402
Glenn Simon <glenncochransimon@gmail.com>
2023-10-27 09:38:31 -0700

    animated hearts

--- public/index.js
+++ public/index.js
@@ -9,18 +9,52 @@ function startHearts() {
   while (heartNum < 25) {
     // horTranslate will be used with 'vw' in css for percentage of view width
     setTimeout(() => {
-      const horTranslate = 0.5 * (Math.random() - 0.5);
+      const horTranslate = 0.75 * (Math.random() - 0.5);
       const heart = document.createElement('img');
       heart.src = 'assets/heart-icon-48.png';
       heart.alt = 'heart';
+      heart.classList = 'heart';
       heart.style.position = 'absolute';
       heart.style.top = '50%';
       heart.style.left = '50%';
       heart.style.transform = 'translate(-50%, -50%)';
       heart.style.zIndex = 0;
       floatingHearts.appendChild(heart);
-      heart.animate([]);
+      heart.animate(
+        [
+          { offset: 0 },
+          {
+            transform: `scale(200%) translate(${100 * horTranslate}vw, -50vh)`,
+            easing: 'ease-in',
+            offset: 1,
+          },
+        ],
+        { duration: 2000, fill: 'forwards' }
+      );
     }, 250 * heartNum);
     heartNum += 1;
   }
+  setTimeout(() => {
+    floatingHearts.innerHTML = '';
+    const heart = document.createElement('img');
+    heart.src = 'assets/heart-icon-48.png';
+    heart.alt = 'heart';
+    heart.classList = 'heart';
+    heart.style.position = 'absolute';
+    heart.style.top = '50%';
+    heart.style.left = '50%';
+    heart.style.transform = 'translate(-50%, -50%)';
+    floatingHearts.appendChild(heart);
+    heart.animate(
+      [
+        { opacity: 0.5, offset: 0 },
+        {
+          transform: 'scale(200%) translate(-25%, -10vh)',
+          easing: 'ease-in',
+          offset: 1,
+        },
+      ],
+      { duration: 2000, fill: 'forwards' }
+    );
+  }, 2000 + 250 * 25);
 }
