c591c012659b0a2f40163dd0012a67ff08f1c538
Glenn Simon <glenncochransimon@gmail.com>
2023-10-29 08:01:45 -0700

    clean up layout

--- public/index.js
+++ public/index.js
@@ -6,11 +6,13 @@ const liftedHeart = document.querySelector('.lifted-heart');
 liftedHeart.style.position = 'relative';
 liftedHeart.style.top = '-50%';
 liftedHeart.style.transform = 'scale(0.5)';
+const numHearts = 15;
 setTimeout(startHearts, 2000);
 
 let heartNum = 0;
 function startHearts() {
-  while (heartNum < 25) {
+  document.querySelector('#splash').remove();
+  while (heartNum < 15) {
     // horTranslate will be used with 'vw' in css for percentage of view width
     setTimeout(() => {
       const horTranslate = 0.75 * (Math.random() - 0.5);
@@ -40,7 +42,7 @@ function startHearts() {
   }
   setTimeout(() => {
     floatingHearts.innerHTML = '';
-  }, 2000 + 250 * 25);
+  }, 2000 + 250 * numHearts);
   setTimeout(() => {
     const heart = document.createElement('img');
     heart.src = 'assets/heart-icon-48.png';
@@ -62,5 +64,5 @@ function startHearts() {
       ],
       { duration: 2000, fill: 'forwards' }
     );
-  }, 500 + 250 * 25);
+  }, 500 + 250 * numHearts);
 }
